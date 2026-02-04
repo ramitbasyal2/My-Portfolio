80b3ed592340638ea3db78cfb9ef4ba672d68cb3
Ramit <basyalramit02@gmail.com>
2026-02-04 23:01:49 +0545

    feature added

--- src/Components/Hobbies/Hobbies.jsx
+++ src/Components/Hobbies/Hobbies.jsx
@@ -3,12 +3,11 @@ import React from 'react'
 const Hobbies = () => {
   
   return (
-    <div className='w-full px-4 sm:px-6 md:px-12 sm:py-46 lg:px-20 py-10 md:py-20 lg:py-40 '>
-      <div className='max-w-7xl mx-auto flex items-center justify-center'>
-        <h1>Hobbies</h1>
-            <div className='relative w-96 h-96 bg-gray-600 mb-2'>
-                 <div><img src="" alt="" /></div>
-                  <div className='absolute w-96 h-96 bg-red-600  -right-36 top-30'><img src="" alt="" /></div>
+    <div className='w-full px-4 sm:px-6 md:px-12 sm:py-46 lg:px-20 py-30 md:py-20 lg:py-40 '>
+      <div className='max-w-7xl mx-auto flex items-center justify-center '>
+            <div className='relative w-66 h-66 bg-gray-600 mb-4'>
+                 <div className=''><img className='w-full h-full object-cover' src="./profile1.jpeg" alt="" /></div>
+                  <div className='absolute w-66 h-64 bg-red-600 -right-10 top-20 '><img src="./project1.png" alt="" /><img src="./project4.png" alt="" /></div>
             </div>
            
       </div>
